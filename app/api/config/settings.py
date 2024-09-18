7a762562f43bbda6dab6755a6536f675e696e1eb
Luis henrique <105991145+LuissHL@users.noreply.github.com>
2024-09-17 21:13:06 -0300

    creating .env

--- app/api/config/settings.py
+++ app/api/config/settings.py
@@ -3,6 +3,5 @@ import os
 class Config:
     SQLACHEMY_TRACK_MODIFICATIONS = True
     SQLALCHEMY_DATABASE_URI = os.getenv(
-        'DATABASE_URL',
-        'mysql+pymysql://username:password@localhost:3306/nome_do_banco'
+        'DATABASE_URL'
     )
